f453222a471efe578535b63369b29b362825fb3d
Hugo Penacho <hugo.penacho@siemens.com>
2021-04-13 20:48:50 +0100

    unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,46 @@
 import React, { useState } from 'react'
 
+const Statistics = (props) => {
+const good = props.feedback[0];
+const neutral = props.feedback[1];
+const bad = props.feedback[2];
+
+  const total = () => {
+    return good + neutral + bad;
+  }
+
+  const average = () => {
+    return (good-bad) / total();
+  }
+
+  const positiveRatio = () =>{ 
+    return (good * 100) / total()
+  }
+
+  return (
+  <>
+  <div>
+        <h3>Feedback Statistics</h3>
+        <p>Good - {good}</p>
+        <p>Neutral - {neutral}</p>
+        <p>Bad - {bad}</p>
+  </div>
+
+      <div>
+        <p> Total Feedback: {total()} </p>
+        <p> Average Feedback: {average()} </p>
+        <p> Positive ratio: {positiveRatio()} % </p>        
+      </div>
+  </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good+neutral+bad;
+  const feedbackArray = [good, neutral, bad];
 
   return (
     <>
@@ -24,16 +59,7 @@ const App = () => {
       </div>
 
       <div>
-        <h3>Feedback Statistics</h3>
-        <p>Good - {good}</p>
-        <p>Neutral - {neutral}</p>
-        <p>Bad - {bad}</p>
-      </div>
-
-      <div>
-        <p> Total Feedback: {total} </p>
-        <p> Average Feedback: {(good - bad) / total} </p>
-        <p> Positive ratio: {(good * 100) / total} % </p>
+        <Statistics feedback={feedbackArray}></Statistics>
         </div>
       
     </>
