d58e9aace4209c1a050bf2fce86381bbc795395d
Hugo Penacho <hugo.penacho@siemens.com>
2021-04-14 20:15:20 +0100

    anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 
 
-const Button = (props) => (
+const NextAnecdoteButton = (props) => (
   <div>
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
   </div>
 )
 
+const VoteButton = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const DisplayVoteCount = (props) => (
+  <p>This joke has {props.votesQty} votes.</p>
+)
+
 const App = () => {
+
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -18,18 +29,29 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
-  const [selected, setSelected] = useState(0)
 
-  function generateRandomNumber (min, max){   
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleVote = (anecdoteIndex) => {
   
-  
+  const newVotes = [...votes]
+  newVotes[anecdoteIndex] += 1
+  setVotes(newVotes);
+  }
+
   return (
     <div>
       {anecdotes[selected]}
-      <Button handleClick={() => setSelected(generateRandomNumber(0,anecdotes.length-1)) } text='Next Anecdote'> </Button>
+      <hr></hr>
+      <NextAnecdoteButton handleClick={() => setSelected(generateRandomNumber(0, anecdotes.length - 1))} text='Next Anecdote'> </NextAnecdoteButton>
+      <VoteButton handleClick={() => handleVote(selected)} text='Vote'> </VoteButton>
+      <DisplayVoteCount votesQty= {votes[selected]}></DisplayVoteCount>
+      <hr></hr>
     </div>
   )
 }
