91490361d40fa32c360e3f3319cac0ce63674459
Hugo Penacho <hugo.penacho@siemens.com>
2021-04-15 00:13:59 +0100

    anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react'
 
+const NextAnecdoteButton = (props) => <button onClick={props.handleClick}> {props.text} </button>
+  
+const VoteButton = (props) => <button onClick={props.handleClick}> {props.text} </button>
 
-const NextAnecdoteButton = (props) => (
-  <div>
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  </div>
-)
+const DisplayVoteCount = (props) => <p>This joke has {props.votesQty} votes.</p>
 
-const VoteButton = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const DisplayTopAnecdote = (props) => {
+  const maxVotes = Math.max(...props.votesArray);
+  const topAnecIndex = props.votesArray.indexOf(maxVotes);
 
-const DisplayVoteCount = (props) => (
-  <p>This joke has {props.votesQty} votes.</p>
-)
+  if(maxVotes > 0)
+    return <p>{props.anecdotesArray[topAnecIndex]}</p> 
+  
+  else
+    return <p>No anectodes have been voted so far.</p>
+}
 
 const App = () => {
 
@@ -30,28 +28,28 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
-  function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  const generateRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 
   const handleVote = (anecdoteIndex) => {
-  
-  const newVotes = [...votes]
-  newVotes[anecdoteIndex] += 1
-  setVotes(newVotes);
+    const newVotes = [...votes];
+    newVotes[anecdoteIndex] += 1;
+    setVotes(newVotes);
   }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h2> Daily Anecdote </h2>
+      <p>{anecdotes[selected]}</p>
       <hr></hr>
       <NextAnecdoteButton handleClick={() => setSelected(generateRandomNumber(0, anecdotes.length - 1))} text='Next Anecdote'> </NextAnecdoteButton>
       <VoteButton handleClick={() => handleVote(selected)} text='Vote'> </VoteButton>
-      <DisplayVoteCount votesQty= {votes[selected]}></DisplayVoteCount>
+      <DisplayVoteCount votesQty={votes[selected]}></DisplayVoteCount>
       <hr></hr>
+      <h2> Most Voted Anecdote </h2>
+      <DisplayTopAnecdote anecdotesArray = {anecdotes} votesArray = {votes}></DisplayTopAnecdote>
     </div>
   )
 }
