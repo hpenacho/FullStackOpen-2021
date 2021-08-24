8838110f8bc874f387188246eb66a2185621d11f
Hugo <hpenacho89@gmail.com>
2021-08-24 23:38:53 +0100

    Phonebook step10

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,12 +1,28 @@
 import Person from "./Person"
+import personService from '../services/Persons'
 
-const Persons = ({ persons, filter }) => {
+
+
+const Persons = ({ persons, filter, setPersons }) => {
+
+    const handleRemoval = (id) => {
+        let confirmation = window.confirm("Do you wish to delete this user?");
+
+        if (confirmation) {
+            personService
+                .remove(id)
+                .then(response => {
+                    setPersons(persons.filter(person => person.id !== id))
+                })
+        }
+    }
 
     return (
         <ul>
             {persons.filter(p => p.name.includes(filter)).map(person =>
                 <div>
                     <Person key={person.id} person={person.name} number={person.number} />
+                    <button onClick={() => handleRemoval(person.id)}> remove </button>
                 </div>
             )}
         </ul>
